4c53ac92598c3315d5c14986f73aac292ebf52e1
vladislav <gradiusalisik@gmail.com>
2019-05-26 16:04:29 +0300

    Updates

---
+++ precache-manifest.ecfb4d31adc1d0036faf78e41b64eae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82141399bda0527db80b",
+    "url": "/Saper/static/js/runtime~main.8b964d28.js"
+  },
+  {
+    "revision": "bb65e69bb764400df2a3",
+    "url": "/Saper/static/js/main.8a35e7e3.chunk.js"
+  },
+  {
+    "revision": "7e4f7fdd3715e6ba4ec2",
+    "url": "/Saper/static/js/2.227df01c.chunk.js"
+  },
+  {
+    "revision": "bb65e69bb764400df2a3",
+    "url": "/Saper/static/css/main.ad06291b.chunk.css"
+  },
+  {
+    "revision": "9c2fb9cdc558648f7da2c943ad0508bf",
+    "url": "/Saper/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/saper/precache-manifest.3358c61828bc0444f453a2e68b4a0a0e.js"
+  "/Saper/precache-manifest.ecfb4d31adc1d0036faf78e41b64eae7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/saper/index.html", {
+workbox.routing.registerNavigationRoute("/Saper/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
